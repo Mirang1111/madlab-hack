7b5b18ff25672447b5f8efe1df9286ab70b2a5ef
Mirang1111 <83184604+Mirang1111@users.noreply.github.com>
2021-10-03 01:20:52 +0530

    Create Knapsack.cpp

--- hacktoberfest2021/Knapsack.cpp
+++ hacktoberfest2021/Knapsack.cpp
@@ -1,26 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int knapsack(int value[],int wt[],int n,int W)
+int dp[100][100];  // dp array
+int k(int wt[] , int val[] , int W , int n)
 {
-    if(n==0 || W==0)
-    {
-        return 0;
-    }
-    if(wt[n-1]>W){
-        return knapsack(value,wt,n-1,W);
-    }
-    return max(knapsack(value,wt,n-1,W-wt[n-1])+value[n-1],knapsack(value,wt,n-1,W));
+	for(int i = 0 ; i<n+1;i++)
+	{
+		for(int j = 0 ; j<W+1;j++)
+		{
+			if(i==0 || j==0)       // base condition in recursion == initialization in the tabulation method
+			{
+				dp[i][j] = 0;
+			}
+			else if(wt[i-1]<=j)   // condition same as recursion , just replace n , W with i , j
+			{
+				dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]] , dp[i-1][j]);
+			}
+			else if(wt[i-1] > j)
+			{
+				dp[i][j] = dp[i-1][j];
+			}
+		}
+	}
+	return dp[n][W];
 }
-
 int main()
 {
-    int wt[]={10,20,30};
-    int value[]={100,50,150};
-    int W=50;
-
-    cout<<knapsack(value,wt,3,W);
-
-    return 0;
-
+	int n;
+	cin>>n;
+	int wt[n];
+	for(int i = 0 ;i<n;i++)
+	{
+		cin>>wt[i];
+	}
+	int val[n];
+	for(int i = 0 ; i<n;i++)
+	{
+		cin>>val[i];
+	}
+	int W;
+	cin>>W;
+	int c = k(wt,val,W,n);
+	cout<<"The maximum profit obtained in the 0/1 knapsack problem is "<<c;
+	return 0;
 }
